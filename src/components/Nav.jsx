0c9ee506db9d6f88886f32b3a7a4363ff4b31662
sruthic324 <sruthic324@gmail.com>
2024-03-06 19:41:12 +0530

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,15 +5,16 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <div>
-        <AppBar> 
+        <AppBar style={{backgroundColor:'black'}}> 
         <Toolbar>
             <Typography sx={{flexGrow:1}} align='left' variant='h5' style={{ color: 'white', fontSize: '24px', fontFamily: 'monospace' }} >
-               Restaurant
+               Tea Talk
               
             </Typography> 
            
                   <Button style={{color:'white'}}>
                      <Link to='/' style={{color:"white" , fontFamily: 'monospace'}}>Home</Link>
+                     
                   </Button>
                   <Button style={{color:'white'}}>
                      <Link to='/l' style={{color:"white" , fontFamily: 'monospace'}}>Login</Link>
