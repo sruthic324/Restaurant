eb54eb60e78427e5b67ecb2277411b5820542d42
sruthic324 <sruthic324@gmail.com>
2024-03-19 19:36:35 +0530

    new

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,26 @@
+import { Button, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+    <br /> <br /><br />
+    <Typography variant='h2' style={{color: 'orange',fontFamily: 'serif',fontSize: '48px',fontWeight: 'bold',}}>Login Page</Typography>
+    <br />
+    <TextField variant='outlined' label='enter username' focused color='secondary' />
+    <br /><br />
+    <TextField variant='outlined' label='Enter password' focused color='secondary' />
+    <br /><br />
+    {/* <TextField label='hello' color='secondary' focused /> */}
+
+   
+    <br />
+    <br />
+    <Button variant='outlined'>Submit</Button>
+    <br /><br /><br /><br />
+    
+
+</div>
   )
 }
 
